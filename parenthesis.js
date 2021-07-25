f30210248bd39ace7cf3e888d61b6ee0a88d2a33
Satish Janakiraman <sajana@microsoft.com>
2021-07-24 23:24:00 -0700

    validates the parenthesis

--- parenthesis.js
+++ parenthesis.js
@@ -1,20 +1,24 @@
-var main = ['(', '(', ')', ')', ')'];
+var main = ['(', ')', ')'];
 var start = 0;
-var end = main.length-1;
-var bool = true;
-while(start <= end) {
-    if(main[start] ==='(' && main[start+1]===')') {
-        start+=2;
-    } else {
-    if(main[start]==='(' && main[end]===')') {
-        start++;
-        end--;
-    }
-        else { bool=false;
-            break; 
+var end = 0+main.length-1;
+var bool = false;
+if(main.length%2) {
+    while(start < end) {
+        if(main[start] ==='(' && main[start+1]===')') {
+            start+=2;
+        } else {
+        if(main[start] ==='(' && main[end] ===')') {
+            start++;
+            end--;
+        }
+            else {
+                break; 
+                
+            }
             
+            }
+        } if (start > end && end > 0) {
+            bool = true;
         }
-        
-        }
-    }
+}
 console.log(bool);
